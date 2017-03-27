32e160dcedc23c59f92167785b4932b3fe98a44a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-03-28 01:58:31 +0900

    :new: EnemyDatabaseクラスを実装

---
+++ Assets/Scripts/EnemyDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class EnemyDatabase : ScriptableObject
+{
+    [SerializeField]
+    private List<Enemy> elements = new List<Enemy>();
+}
