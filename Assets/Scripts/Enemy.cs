bbd9d1377a762a13ad52dde3da2dcf75b9879ff5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-03-28 01:57:45 +0900

    :new: Enemyクラスを実装

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Enemy
+{
+	/// <summary>
+	/// 名前
+	/// </summary>
+    public string Name;
+
+	/// <summary>
+	/// 素点を加算するか
+	/// </summary>
+	/// <remarks>
+	/// 中ボスやボスは加算されないのでその場合は<c>false</c>にする
+	/// </remarks>
+    public bool addPoint = true;
+
+    /// <summary>
+    /// 素点
+    /// </summary>
+    public List<int> Points;
+}
