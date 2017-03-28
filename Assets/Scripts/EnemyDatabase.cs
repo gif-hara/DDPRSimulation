7717de694704ab5effa37ee4d2116b6ab345f029
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-03-28 11:50:25 +0900

    :new: 重複チェックを実装

--- Assets/Scripts/EnemyDatabase.cs
+++ Assets/Scripts/EnemyDatabase.cs
@@ -1,10 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu()]
 public class EnemyDatabase : ScriptableObject
 {
     [SerializeField]
     private List<Enemy> elements = new List<Enemy>();
+
+    [ContextMenu("重複チェック")]
+    private void OverlapCheck()
+    {
+        var overlaps = this.elements.GroupBy(e => e.Name)
+            .Where(e => e.Count() > 1)
+            .Select(e => e.FirstOrDefault()).ToList();
+
+        if(overlaps.Count <= 0)
+        {
+            Debug.Log("重複していませんでした！");
+        }
+        else
+        {
+            overlaps.ForEach(o => Debug.LogErrorFormat("{0}が重複しています", o.Name));
+        }
+    }
 }
